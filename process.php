f01cb7aa906f624e760760f26e9f9f2ba5770b17
Hendriyan Ogivano <ogivano06@gmail.com>
2019-04-07 18:34:08 +0700

    ketentuan ber be selsai

--- process.php
+++ process.php
@@ -1,11 +1,11 @@
 <?php
 include './ehcs.php';
 $ehcs = new Ehcs();
-// if ($ehcs->checkStem('nyanyi')){
-//   echo 'true';
-// } else {
-//   echo 'false';
-// }
+if ($ehcs->checkStem('nyanyi')){
+  echo 'true';
+} else {
+  echo 'false';
+}
 
-echo $ehcs->checkParticle("berelah");
+echo $ehcs->checkParticle("blah")['stem'];
 ?>
